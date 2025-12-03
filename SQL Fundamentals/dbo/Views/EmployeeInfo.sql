c3bd3ba9d272981412761a427792dec0802eb02c
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-12-03 22:30:14 +0500

    Task 2: add EmplyeeInfo view

---
+++ SQL Fundamentals/dbo/Views/EmployeeInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS
+	SELECT 
+		[employee].[Id] AS EmplyeeId, 
+		ISNULL([employee].[EmployeeName], [person].[FirstName] + ' ' + [person].[LastName]) as EmpoyeeFullName,
+		[address].[ZipCode] + '_' + [address].[State] + ', ' + [address].[City] + '-' + [address].[Street] as EmplyeeFullAddress,
+		[employee].[CompanyName] + '(' + [employee].[Position] + ')' as EmployeeCompanyInfo
+	FROM dbo.Employee employee
+	LEFT JOIN dbo.Address address ON [employee].[AddressId] = [address].[Id]
+	LEFT JOIN dbo.Person person ON [employee].[PersonId] = [person].[Id]
