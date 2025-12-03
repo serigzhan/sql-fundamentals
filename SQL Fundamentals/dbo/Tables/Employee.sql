2744e73d1315d5e887ed232bc7c0ee33176dc738
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-12-03 21:09:02 +0500

    Add project files.

---
+++ SQL Fundamentals/dbo/Tables/Employee.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AddressId] INT NOT NULL, 
+    [PersonId] INT NOT NULL, 
+    [CompanyName] NVARCHAR(20) NOT NULL, 
+    [Position] NVARCHAR(30) NULL, 
+    [EmployeeName] NVARCHAR(100) NULL, 
+    CONSTRAINT [FK_Employee_ToAddress] FOREIGN KEY ([AddressId]) REFERENCES [Address]([Id]), 
+    CONSTRAINT [FK_Employee_ToPerson] FOREIGN KEY ([PersonId]) REFERENCES [Person]([Id])
+)
