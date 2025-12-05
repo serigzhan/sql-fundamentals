072dd1118ecbb63144eb36ff4759a4b8203e0183
Serigzhan Naviy <serigzhan_naviy@epam.com>
2025-12-05 14:55:43 +0500

    Task 4: create trigger after employee created

---
+++ SQL Fundamentals/dbo/Triggers/AfterEmployeeCreated.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER [AfterEmployeeCreated]
+ON dbo.Employee
+AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO dbo.Company(AddressId, Name)
+	SELECT employee.AddressId, employee.CompanyName
+	FROM inserted employee;
+END;
